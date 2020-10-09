c2b0ab80920f64c6faf6a53eab5885ef8d62a3be
Sairaj Kodilkar <skodilkar7@gmali.com>
2020-10-09 20:37:21 +0530

    project started

---
+++ num.h
@@ -0,0 +1,33 @@
+/* 
+ * Data structure for number
+ */
+
+
+#ifndef _NUM
+#define _NUM
+
+#include <stdio.h>
+
+// Max digit length in one node
+#define DIG_LEN 8 
+
+typedef struct node{
+	int digit;
+	struct node *next;
+}node;
+
+typedef struct num{
+	node *part;
+	int point;
+	int sign;
+}num;
+
+num add(num one ,num two);
+num sub(num one, num two);
+num multiply(num one, num two);
+num divide(num one, num two);
+
+#endif 
+
+
+
---
+++ parse.h
@@ -0,0 +1,18 @@
+#ifndef _PARSE
+#define _PARSE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limit.h>
+#include <string.h>
+#include "num.h"
+
+typedef struct token{
+	int type;
+	union{
+		num number;
+		char op;
+	}data;
+};
+
+#endif 
