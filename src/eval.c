41ffbc0ee9cb1012e2c248f57e3833c215001fc2
Sairaj Kodilkar <skodilkar7@gmali.com>
2020-10-24 02:10:51 +0530

    major bug fixed

--- src/eval.c
+++ src/eval.c
@@ -1,9 +1,30 @@
 #include "eval.h"
+enum precedence {LOW, BRACKET, ADDSUB, MULDIV, MODULO};
 
-enum precedence {LOW, ADDSUB, MULDIV, MODULO, BRACKET};
+int getprece(char ch){
+	switch(ch){
+		case '+': case '-':
+			return ADDSUB;
+		case '*': case '/':
+			return MULDIV;
+		case '%':
+			return MODULO;
+		case '(': case ')':
+			return BRACKET;
+	}
+	return LOW;
+}
 
-void cleannstack(nstack *);
-/* TODO division operation is pending */
+void cleannstack(nstack *numbers){
+	num one;
+	while(!nisempty(numbers)){
+		one = npop(numbers);
+		erasenum(&one);
+	}
+	return;
+}
+
+/* todo division operation is pending */
 num eval(char *str){
 	nstack numbers;
 	cstack operators;
@@ -11,8 +32,8 @@ num eval(char *str){
 	token t;
 	num result, one, two, error;
 
-	int currpre = LOW, prevpre, bracket = 0;
-	char op;
+	int currpre = LOW, prevpre ;
+	static char op;
 
 	initnum(&one);
 	initnum(&two);
@@ -26,138 +47,119 @@ num eval(char *str){
 		switch(t.type){
 			case OPERATOR:
 				prevpre = currpre;
-				switch(t.data.op){
-					case '+': case '-':
-						currpre = ADDSUB + bracket;
-						break;
-					case '*': case '/':
-						currpre = MULDIV + bracket;
-						break;
-					case '%':
-						currpre = MODULO + bracket;
-						break;
-					case '(':
-						bracket += BRACKET;
-						cpush(&operators, t.data.op);
-						continue;
-						break;
-					case ')':
-						bracket -= BRACKET;
-						continue;
-						break;
-					default:
-						return error;
+				currpre = getprece(t.data.op);
+				if(currpre == BRACKET && t.data.op == '('){
+					cpush(&operators, t.data.op);
+					break;
 				}
-				if(currpre <= prevpre){
-					while(!cisempty(&operators)){
-						op = cpop(&operators);
-						if(op == '('){
-							break;
-						}
-						if(nisempty(&numbers))
-							return error;
+				else if(currpre == BRACKET && t.data.op == ')'){
+					while((op = cpop(&operators)) != '('){
 						two = npop(&numbers);
-						if(nisempty(&numbers))
-							return error;
 						one = npop(&numbers);
-
 						switch(op){
 							case '+':
 								result = add(one, two);
 								break;
 							case '-':
-								result= sub(one, two);
+								result = sub(one, two);
 								break;
 							case '*':
 								result = multiply(one, two);
 								break;
 							case '/':
-								//result = divide(one, two);
 								break;
 							case '%':
-								//result = modulo(one, two);
 								break;
 							default:
+								cleannstack(&numbers);
 								return error;
 								break;
 						}
-						/* erasing number that has been popped */
-						erasenum(&one);
 						erasenum(&two);
-						initnum(&one);
+						erasenum(&one);
 						initnum(&two);
-						if(nisfull(&numbers)){
-							cleannstack(&numbers);
-							return error;
-						}
+						initnum(&one);
 						npush(&numbers, result);
 					}
+					break;
 				}
-				cpush(&operators, t.data.op);
-				break;
-
-			case NUMBER:
-				if(nisfull(&numbers)){
-					cleannstack(&numbers);
-					return error;
-				}
-				npush(&numbers, t.data.number);
-				break;
-
-			case END:
-				if(cisempty(&operators) || bracket != 0)
-					return error;
-
-				while(!cisempty(&operators)){
-					op = cpop(&operators);
-					if(op == '('){
-						continue;
-					}
-
-					if(nisempty(&numbers))
-						return error;
+				else if(currpre < prevpre){
 					two = npop(&numbers);
-					if(nisempty(&numbers))
-						return error;
 					one = npop(&numbers);
-
+					op = cpop(&operators);
 					switch(op){
 						case '+':
 							result = add(one, two);
 							break;
 						case '-':
-							result= sub(one, two);
+							result = sub(one, two);
 							break;
 						case '*':
 							result = multiply(one, two);
 							break;
 						case '/':
-							//result = divide(one, two);
 							break;
 						case '%':
-							//result = modulo(one, two);
 							break;
 						default:
+							cleannstack(&numbers);
+							break;
+					}
+					npush(&numbers, result);
+					cpush(&operators, t.data.op);
+					erasenum(&two);
+					erasenum(&one);
+					initnum(&two);
+					initnum(&one);
+					break;
+				}
+				else{
+					cpush(&operators, t.data.op);
+					break;
+				}
+			case NUMBER:
+				npush(&numbers, t.data.number);
+				break;
+
+			case END:
+				while(!cisempty(&operators)){
+					two = npop(&numbers);
+					one = npop(&numbers);
+					op = cpop(&operators);
+					switch(op){
+						case '+':
+							result = add(one, two);
+							break;
+						case '-':
+							result = sub(one, two);
+							break;
+						case '*':
+							result = multiply(one, two);
+							break;
+						case '/':
+							break;
+						case '%':
+							break;
+						default:
+							cleannstack(&numbers);
 							return error;
 							break;
 					}
-					/* erasing number that has been popped */
-					erasenum(&one);
-					erasenum(&two);
-					initnum(&one);
-					initnum(&two);
 					npush(&numbers, result);
+					erasenum(&two);
+					erasenum(&one);
+					initnum(&two);
+					initnum(&one);
 				}
-				if(nisempty(&numbers) || bracket != 0){
-					return error;
-				}
-				result = npop(&numbers);
-				return result;
+				return npop(&numbers);
 				break;
+
 			case ERR:
 				cleannstack(&numbers);
 				return error;
 				break;
+
+
 			default:
 				cleannstack(&numbers);
 				return error;
@@ -167,14 +169,6 @@ num eval(char *str){
 	}
 }
 
-void cleannstack(nstack *numbers){
-	num one;
-	while(!nisempty(numbers)){
-		one = npop(numbers);
-		erasenum(&one);
-	}
-	return;
-}
 
 
 
